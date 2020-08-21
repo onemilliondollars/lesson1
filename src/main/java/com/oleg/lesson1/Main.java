17c975c330792c8d47af7b7038daad1b4af38b14
Olegs Kovaluks <olegs.kovaluks@gmail.com>
2020-08-20 17:01:31 -0700

    Main class coment

--- src/main/java/com/oleg/lesson1/Main.java
+++ src/main/java/com/oleg/lesson1/Main.java
@@ -5,23 +5,6 @@ import com.oleg.lesson1.service.SimpleCalculatorService;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
-        SimpleCalculatorService simpleCalculatorService = new SimpleCalculatorService(simpleCalculator);
-        System.out.println(simpleCalculatorService.dev(10, 3));
+        // App
     }
-//    public static int sum(int a, int b){
-//        return a + b;
-//    }
-//
-//    public static int sub(int a, int b){
-//        return a - b;
-//    }
-//
-//    public static int multi(int a, int b){
-//        return a * b;
-//    }
-//
-//    public static float dev(float a, float b){
-//        return a / b;
-//    }
 }
